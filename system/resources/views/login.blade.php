e2ef6835b136047fa296b286e4378fbd48563ae6
jotavital <80636554+jotavital@users.noreply.github.com>
2021-09-08 17:45:24 -0300

    little update

--- system/resources/views/login.blade.php
+++ system/resources/views/login.blade.php
@@ -8,7 +8,7 @@
     <div class="col-md-5 d-flex justify-content-center flex-column">
         <h1 class="mb-4 d-flex justify-content-center">Login - nomeEmpresa</h1>
 
-        <form action="#" method="post">
+        <form action="#" method="get">
             <div class="mb-3 form-outline">
               <input type="text" id="usuario" class="form-control"/>
               <label class="form-label" for="usuario">Usuário</label>
@@ -17,9 +17,6 @@
               <input type="password" id="senha" class="form-control"/>
               <label class="form-label" for="senha">Senha</label>
             </div>
-            <!-- <div class="mb-3 d-flex justify-content-center">
-              <small>Ainda não tem um login? <a href="register">Cadastre-se aqui.</a></small>
-            </div> -->
             <div class="d-flex justify-content-center">
                 <button type="submit" class="btn btn-success btn-rounded">Entrar</button>
             </div>
