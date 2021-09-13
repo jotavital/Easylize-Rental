dad2ab0fdd6c3ff22129c5ab80c36dd002f376f0
jota vital <joaopedrovitaldoctum@gmail.com>
2021-09-13 19:25:33 -0300

    feat: :bug: revert

--- system/storage/framework/views/ff001f4b5b0d393436d7b5161821de686faf6222.php
+++ system/storage/framework/views/ff001f4b5b0d393436d7b5161821de686faf6222.php
@@ -8,7 +8,7 @@
     <div class="col-md-5 d-flex justify-content-center flex-column">
         <h1 class="mb-4 d-flex justify-content-center">Login - nomeEmpresa</h1>
 
-        <form action="#" method="get">
+        <form action="#" method="post">
             <div class="mb-3 form-outline">
               <input type="text" id="usuario" class="form-control"/>
               <label class="form-label" for="usuario">Usuário</label>
@@ -17,6 +17,9 @@
               <input type="password" id="senha" class="form-control"/>
               <label class="form-label" for="senha">Senha</label>
             </div>
+            <!-- <div class="mb-3 d-flex justify-content-center">
+              <small>Ainda não tem um login? <a href="register">Cadastre-se aqui.</a></small>
+            </div> -->
             <div class="d-flex justify-content-center">
                 <button type="submit" class="btn btn-success btn-rounded">Entrar</button>
             </div>
