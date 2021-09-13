58482af6ff39f47d30bbca99909ed41fedf834b0
jota vital <joaopedrovitaldoctum@gmail.com>
2021-09-13 19:36:03 -0300

    feat: :fire: added .env.example

---
+++ system/storage/framework/views/49942f7786a63d65eaea61a84c2bc74413fcdd5f.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/app.css">
+
+    <title>Easylize Rental - <?php echo $__env->yieldContent('title'); ?></title>
+</head>
+
+<body>
+
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <footer>
+        <small style="float:right;"><p>Powered by Easylize Rental team - 2021. Todos os direitos reservados©</p></small>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.6.0/mdb.min.js"></script>
+    </footer>
+</body>
+
+</html><?php /**PATH /home/jotavital/Desktop/Easylize-Rental/system/resources/views/layouts/main.blade.php ENDPATH**/ ?>
