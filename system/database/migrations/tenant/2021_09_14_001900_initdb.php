e906e6bec184dba61191c7532443c3fa7430be14
jotavital <80636554+jotavital@users.noreply.github.com>
2021-09-16 18:24:08 -0300

    feat: :zap: iniciando multi tenancy

---
+++ system/database/migrations/tenant/2021_09_14_001900_initdb.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class Initdb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // $sql = file_get_contents(base_path() . "/database/sql/easylize_rental_db.sql");
+        // DB::unprepared($sql);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
